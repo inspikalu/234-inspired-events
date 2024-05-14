337fefe50f8251ec6cec9a08923ced2b541fa153
marcuscharles-code <charlesfatunsin@gmail.com>
2024-05-14 14:59:16 +0200

    Update App.jsx and NavBar.jsx
    - Remove import statements for reactLogo and viteLogo in App.jsx
    - Add semicolon to export default statement in NavBar.jsx
    - Add

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import NavBar from './Components/Navbar/NavBar'
-
 
 function App() {
  
@@ -13,4 +9,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- vite-project/src/Components/Navbar/NavBar.jsx
+++ vite-project/src/Components/Navbar/NavBar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import './NavBar.css'
+import './NavBar.css';
+
 function NavBar() {
     return (
-
         <nav className='navbar'>
-            <div><img src="./img/Group 238419.png" alt="logo" /></div>
+            <div>
+                <img src="./img/Group 238419.png" alt="logo" />
+            </div>
 
             <ul>
                 <li>Home</li>
@@ -16,8 +17,7 @@ function NavBar() {
 
             <button className='navbar-btn'>Book A Call</button>
         </nav>
-
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
