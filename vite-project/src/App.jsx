5d9e63eb24781349edd2fd039db0d904f24328ee
inspikalu <inspikalu@gmail.com>
2024-05-17 13:51:51 +0100

    Initial Commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/Home'; 
-import Services from './routes/Services'; 
-import About from './routes/About'; 
-import Gallery from './routes/Gallery'; 
-import Contact from './routes/Contact'; 
+import Home from './routes/Home';
+import Services from './routes/Services';
+import About from './routes/About';
+import Gallery from './routes/Gallery';
+import Contact from './routes/Contact';
 
 function App() {
- 
+
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} /> 
-          <Route path='/services' element={<Services />} /> 
-          <Route path='/about' element={<About />} /> 
-          <Route path='/gallery' element={<Gallery />} /> 
-          <Route path='/contact' element={<Contact />} /> 
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router>
     </>
--- vite-project/src/routes/About.jsx
+++ vite-project/src/routes/About.jsx
@@ -1,6 +1,7 @@
 import HeroImg2 from '../components/HeroImg2';
 import NavBar from '../components/NavBar';
 import IntroImg from '/about.webp';
+import PhoneImg from '../assets/phone.svg'
 
 const About = () => {
   return (
@@ -9,6 +10,42 @@ const About = () => {
       <main>
         <HeroImg2 heading='About Us' image={IntroImg} />
       </main>
+     <section className="aboutBody">
+     <section className="whoAreWe">
+        <h2 className='headerRedText'>Who are we</h2>
+        <p>
+          <span className='greenText'>234 Inspired</span> is a visionary brand entity dedicated to curtailing unforgettable experiences as the leading event management, rentals, and travel and tours company.
+        </p>
+        <p>
+          Established in 2018. with firm roots in the United Kingdom and a thriving branch office in Nigeria, we have swiftly ascended to the forefront of our industries. Officailly registered with the UK authorities in 2020 our journey is marked by a commitment to excelence and innovation. We are the Luxe Event Connoisserus
+        </p>
+      </section>
+      <section>
+        <h2 className="headerRedText">Our Mission</h2>
+        <p>
+          Our mission is to craft exceptional events, provide top-tier event rentals, and deliver unforgettable travel experieces, all while exceeding client expectiations and enriching lives
+        </p>
+      </section>
+
+      <section>
+        <h2 className="headerRedText">Our Vision</h2>
+        <p>To be the epitome of sophistication and excellence in the events management, event rentals and travel and tours industries, setting new standards for luxury and innovation.</p>
+      </section>
+
+      <footer>
+        <span>24/7 Guest Communication</span>
+        <p>
+          Our team serves as your digital front desk, handling guest inquireis, booking confirmations and providding 24/7 support
+        </p>
+
+        <button>
+          <div>
+            <img src={PhoneImg} alt="Phone image" />
+            <span>Talk to Us</span>
+          </div>
+        </button>
+      </footer>
+     </section>
     </>
   )
 }
