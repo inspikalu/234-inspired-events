2a3ee62b0466d21bdf008b4a14ae458f90c3cc3d
inspikalu <inspikalu@gmail.com>
2024-05-19 11:15:28 +0100

    Contact form styled

--- vite-project/src/routes/About.jsx
+++ vite-project/src/routes/About.jsx
@@ -2,6 +2,8 @@ import HeroImg2 from '../components/HeroImg2';
 import NavBar from '../components/NavBar';
 import IntroImg from '/about.webp';
 import PhoneImg from '../assets/phone.svg'
+import Line from "../assets/Vector-1.svg"
+import Line2 from "../assets/Vector.svg"
 
 const About = () => {
   return (
@@ -10,29 +12,30 @@ const About = () => {
       <main>
         <HeroImg2 heading='About Us' image={IntroImg} />
       </main>
-     <section className="aboutBody">
-     <section className="whoAreWe">
-        <h2 className='headerRedText'>Who are we</h2>
-        <p>
-          <span className='greenText'>234 Inspired</span> is a visionary brand entity dedicated to curtailing unforgettable experiences as the leading event management, rentals, and travel and tours company.
-        </p>
-        <p>
-          Established in 2018. with firm roots in the United Kingdom and a thriving branch office in Nigeria, we have swiftly ascended to the forefront of our industries. Officailly registered with the UK authorities in 2020 our journey is marked by a commitment to excelence and innovation. We are the Luxe Event Connoisserus
-        </p>
-      </section>
-      <section>
-        <h2 className="headerRedText">Our Mission</h2>
-        <p>
-          Our mission is to craft exceptional events, provide top-tier event rentals, and deliver unforgettable travel experieces, all while exceeding client expectiations and enriching lives
-        </p>
-      </section>
+      <section className="aboutBody">
+        <section className="whoAreWe">
+          <h2 className='headerRedText'>Who are we</h2>
+          <p>
+            <span className='greenText'>234 Inspired</span> is a visionary brand entity dedicated to curtailing unforgettable experiences as the leading event management, rentals, and travel and tours company.
+          </p>
+          <p>
+            Established in 2018. with firm roots in the United Kingdom and a thriving branch office in Nigeria, we have swiftly ascended to the forefront of our industries. Officailly registered with the UK authorities in 2020 our journey is marked by a commitment to excelence and innovation. We are the Luxe Event Connoisserus
+          </p>
+        </section>
+        <section>
+          <h2 className="headerRedText">Our Mission</h2>
+          <p>
+            Our mission is to craft exceptional events, provide top-tier event rentals, and deliver unforgettable travel experieces, all while exceeding client expectiations and enriching lives
+          </p>
+        </section>
 
-      <section>
-        <h2 className="headerRedText">Our Vision</h2>
-        <p>To be the epitome of sophistication and excellence in the events management, event rentals and travel and tours industries, setting new standards for luxury and innovation.</p>
-      </section>
+        <section>
+          <h2 className="headerRedText">Our Vision</h2>
+          <p>To be the epitome of sophistication and excellence in the events management, event rentals and travel and tours industries, setting new standards for luxury and innovation.</p>
+        </section>
 
-      <footer>
+      </section>
+      <footer className='sectionFooter'>
         <span>24/7 Guest Communication</span>
         <p>
           Our team serves as your digital front desk, handling guest inquireis, booking confirmations and providding 24/7 support
@@ -44,8 +47,11 @@ const About = () => {
             <span>Talk to Us</span>
           </div>
         </button>
+        <img src={Line} alt="" className="line line1" />
+        <img src={Line2} alt="" className="line line2" />
+        <div className="line line3"></div>
+        <div className="line line4"></div>
       </footer>
-     </section>
     </>
   )
 }
--- vite-project/src/routes/Contact.jsx
+++ vite-project/src/routes/Contact.jsx
@@ -1,7 +1,8 @@
 import HeroImg2 from '../components/HeroImg2';
 import NavBar from '../components/NavBar';
 import IntroImg from '/contact.webp';
-
+import UploadDocumentIcon from "../assets/Upload.svg"
+import AddImageIcon from "../assets/addImage.png"
 const Contact = () => {
   return (
     <>
@@ -9,6 +10,36 @@ const Contact = () => {
       <main>
         <HeroImg2 heading='Contact Us' image={IntroImg} />
       </main>
+      <section className='contact'>
+        <form action="">
+          <label htmlFor="fullName">Full Name</label>
+          <input type="text" name='fullName' id='fullName' placeholder='Input Full Name' />
+
+          <label htmlFor="email">Email Adress</label>
+          <input type="email" name='email' id='email' placeholder='Input Email Address' />
+
+          <label htmlFor="eventDescription">Event Description</label>
+          <textarea name="eventDescription" id="eventDescription" placeholder='Event Description'></textarea>
+          <div>
+            <label>
+              <img src={UploadDocumentIcon} alt="Upload Icon" />
+              <span>Upload Document</span>
+              <input type="file" accept="application/*" className='hidden' />
+            </label>
+            <label>
+              <img src={AddImageIcon} alt="Add Image Icon" />
+              <span>Add Image</span>
+              <input type="file" accept='image/*' className='hidden' />
+            </label>
+          </div>
+
+          <button>Create Event</button>
+        </form>
+
+        <div className="circle circle1"></div>
+        <div className="circle circle2"></div>
+        <div className="circle circle3"></div>
+      </section>
     </>
 
   )
